149501e435ab3e54dd04b3adc124e89cfc4b7769
user530 <m.timarsuev@gmail.com>
2024-04-23 21:21:21 +0300

    Cleared up name for entities

--- src/database/entities/child.entity.ts
+++ src/database/entities/child.entity.ts
@@ -2,7 +2,7 @@ import { Entity, JoinColumn, ManyToOne } from 'typeorm';
 import { Person } from './person.entity';
 import { User } from './user.entity';
 
-@Entity({ name: 'child' })
+@Entity()
 export class Child extends Person {
     // In this implementation we separate children from users and I decided not to keep children data for deleted user
     @ManyToOne(() => User, user => user.children, { onDelete: 'CASCADE' })
--- src/database/entities/user.entity.ts
+++ src/database/entities/user.entity.ts
@@ -2,7 +2,7 @@ import { Entity, OneToMany } from 'typeorm';
 import { Person } from './person.entity';
 import { Child } from './child.entity';
 
-@Entity({ name: 'user' })
+@Entity({ name: 'api_user' })
 export class User extends Person {
     @OneToMany(() => Child, child => child.parent)
     children: Child[];
