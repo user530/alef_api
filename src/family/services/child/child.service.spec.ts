fd4a97b0f8b08dbedc1820bb690a6db77121ef6f
user530 <m.timarsuev@gmail.com>
2024-04-24 20:34:14 +0300

    Unit tests for User and Child services

--- src/family/services/child/child.service.spec.ts
+++ src/family/services/child/child.service.spec.ts
@@ -1,12 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ChildService } from './child.service';
+import { Repository } from 'typeorm';
+import { Child, User } from '../../entities';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { BadRequestException, NotFoundException, UnauthorizedException } from '@nestjs/common';
 
 describe('ChildService', () => {
   let service: ChildService;
+  let mockUserRepository: Partial<Record<keyof Repository<User>, jest.Mock>>;
+  let mockChildRepository: Partial<Record<keyof Repository<Child>, jest.Mock>>;
 
   beforeEach(async () => {
+    // Implement mock repository methods
+    mockUserRepository = {
+      findOne: jest.fn(),
+      findOneBy: jest.fn(),
+    };
+
+    mockChildRepository = {
+      find: jest.fn(),
+      findOne: jest.fn(),
+      create: jest.fn(),
+      save: jest.fn(),
+      preload: jest.fn(),
+      delete: jest.fn(),
+    }
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ChildService],
+      providers: [
+        ChildService,
+        {
+          provide: getRepositoryToken(Child),
+          useValue: mockChildRepository,
+        },
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<ChildService>(ChildService);
@@ -15,4 +46,327 @@ describe('ChildService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('getChildrenForUser', () => {
+    it('should return all children for a given user', async () => {
+      // Mockup data
+      const userId = 1;
+      const expectedChildren = [
+        {
+          id: 1,
+          firstName: 'Маша',
+          lastName: 'Пупкина',
+          fatherName: 'Васильевна',
+          age: 13
+        },
+        {
+          id: 2,
+          firstName: 'Семён',
+          lastName: 'Пупкин',
+          fatherName: 'Васильевич',
+          age: 14
+        },
+      ];
+
+      // Emulate repostitory returns
+      mockUserRepository.findOneBy.mockResolvedValue({ id: userId });
+      mockChildRepository.find.mockResolvedValue(expectedChildren);
+
+      // Method call result
+      const result = await service.getChildrenForUser(userId);
+
+      // Check that service returns what repository provides, and verify that it was called with right arguments
+      expect(result).toEqual(expectedChildren);
+      expect(mockUserRepository.findOneBy).toHaveBeenCalledWith({ id: userId });
+      expect(mockChildRepository.find).toHaveBeenCalledWith({ where: { parent: { id: userId } } });
+    });
+
+    it('should throw NotFoundException if user does not exist', async () => {
+      mockUserRepository.findOneBy.mockResolvedValue(null);
+      await expect(service.getChildrenForUser(99)).rejects.toThrow(NotFoundException);
+    });
+  });
+
+  describe('addChildToUser', () => {
+    it('should add a child to a user', async () => {
+      // Mockup data
+      const createChildDto = {
+        firstName: 'Пётр',
+        lastName: 'Пупкин',
+        fatherName: 'Васильевич',
+        age: 13
+      };
+      const parentUser = {
+        id: 1,
+        firstName: 'Вася',
+        lastName: 'Пупкин',
+        fatherName: 'Петрович',
+        age: 30,
+        children: 2,
+      };
+      const savedChild = {
+        ...createChildDto,
+        id: 3,
+        parent: parentUser,
+        createdAt: new Date(Date.now() - 1000 * 60 * 60 * 24),
+        updatedAt: new Date(Date.now() - 1000 * 60 * 60 * 24),
+      };
+
+      // Emulate repostitory returns
+      mockUserRepository.findOne.mockResolvedValue(parentUser);
+      mockChildRepository.create.mockReturnValue(savedChild);
+      mockChildRepository.save.mockResolvedValue(savedChild);
+
+      // Method call result
+      const result = await service.addChildToUser(parentUser.id, createChildDto);
+
+      // Check that service returns what repository provides, and verify that it was called with right arguments
+      expect(result).toEqual(savedChild);
+      expect(mockUserRepository.findOne)
+        .toHaveBeenCalledWith({
+          where: { id: parentUser.id },
+          relations: ['children'],
+        });
+
+      expect(mockChildRepository.create)
+        .toHaveBeenCalledWith({
+          ...createChildDto,
+          parent: parentUser,
+        });
+
+      expect(mockChildRepository.save).toHaveBeenCalledWith(savedChild);
+    });
+
+    it('should throw BadRequestException if child count exceeds limit', async () => {
+      const parentUser = {
+        id: 1,
+        firstName: 'Вася',
+        lastName: 'Пупкин',
+        fatherName: 'Петрович',
+        age: 30,
+        children: new Array(5).fill({}),
+      };
+
+      mockUserRepository.findOne.mockResolvedValue(parentUser);
+
+      await expect(
+        service
+          .addChildToUser(1, {
+            firstName: 'Елизавета',
+            lastName: 'Пупкина',
+            fatherName: 'Васильевна',
+            age: 3
+          }))
+        .rejects.toThrow(BadRequestException);
+    });
+
+    it('should throw BadRequestException if child age is not suitable', async () => {
+      const parentUser = {
+        id: 1,
+        firstName: 'Вася',
+        lastName: 'Пупкин',
+        fatherName: 'Петрович',
+        age: 30,
+        children: new Array(2).fill({}),
+      };
+
+      mockUserRepository.findOne.mockResolvedValue(parentUser);
+
+      await expect(
+        service
+          .addChildToUser(1, {
+            firstName: 'Евлампий',
+            lastName: 'Пупкин',
+            fatherName: 'ВасильевичЪ',
+            age: 103
+          }))
+        .rejects.toThrow(BadRequestException);
+    });
+  });
+
+  describe('updateChild', () => {
+    it('should update a child if it exists and is related to the user', async () => {
+      const userId = 1;
+      const updateChildDTO = { age: 4 };
+      const existingChild = {
+        id: 5,
+        firstName: 'Елизавета',
+        lastName: 'Пупкина',
+        fatherName: 'Васильевна',
+        age: 3,
+        createdAt: new Date(Date.now() - 1000 * 60 * 60 * 24),
+        updatedAt: new Date(Date.now() - 1000 * 60 * 60 * 24),
+        parent: { id: userId }
+      };
+
+      mockChildRepository.findOne.mockResolvedValue(existingChild);
+      mockChildRepository.preload.mockResolvedValue({
+        ...existingChild,
+        ...updateChildDTO,
+        updatedAt: new Date(),
+      });
+      mockChildRepository.save.mockResolvedValue({
+        ...existingChild,
+        ...updateChildDTO,
+      });
+
+      const result = await service.updateChild(userId, existingChild.id, updateChildDTO);
+
+      expect(result).toEqual({ ...existingChild, ...updateChildDTO });
+      expect(mockChildRepository.preload)
+        .toHaveBeenCalledWith({
+          ...updateChildDTO,
+          updatedAt: expect.any(Date),
+          createdAt: existingChild.createdAt,
+          id: existingChild.id
+        });
+    });
+
+    it('should throw BadRequestException if updated age is not suitable', async () => {
+      const parentUser = {
+        id: 1,
+        firstName: 'Вася',
+        lastName: 'Пупкин',
+        fatherName: 'Петрович',
+        age: 30,
+        children: new Array(2).fill({}),
+      };
+      const existingChild = {
+        id: 5,
+        firstName: 'Елизавета',
+        lastName: 'Пупкина',
+        fatherName: 'Васильевна',
+        age: 3,
+        createdAt: new Date(Date.now() - 1000 * 60 * 60 * 24),
+        updatedAt: new Date(Date.now() - 1000 * 60 * 60 * 24),
+        parent: parentUser,
+      };
+
+      mockChildRepository.findOne.mockResolvedValue(existingChild);
+
+      await expect(
+        service
+          .updateChild(
+            parentUser.id,
+            existingChild.id,
+            { age: parentUser.age + 1 }
+          ))
+        .rejects.toThrow(BadRequestException);
+    });
+
+    it('should throw NotFoundException if the child does not exist or not related to user', async () => {
+      mockChildRepository.findOne.mockResolvedValue(null);
+
+      await expect(service.updateChild(1, 99, { age: 10 })).rejects.toThrow(NotFoundException);
+    });
+  });
+
+  describe('deleteChild', () => {
+    it('should delete the child if it exists and is related to the user', async () => {
+      const parentUser = {
+        id: 1,
+        firstName: 'Вася',
+        lastName: 'Пупкин',
+        fatherName: 'Петрович',
+        age: 30,
+        children: new Array(2).fill({}),
+      };
+      const existingChild = {
+        id: 5,
+        firstName: 'Елизавета',
+        lastName: 'Пупкина',
+        fatherName: 'Васильевна',
+        age: 3,
+        createdAt: new Date(Date.now() - 1000 * 60 * 60 * 24),
+        updatedAt: new Date(Date.now() - 1000 * 60 * 60 * 24),
+        parent: parentUser,
+      };
+
+      mockChildRepository.findOne.mockResolvedValue(existingChild);
+      mockChildRepository.delete.mockResolvedValue({ affected: 1 });
+
+      await service.deleteChild(parentUser.id, existingChild.id);
+      expect(mockChildRepository.delete).toHaveBeenCalledWith(existingChild.id);
+    });
+
+    it('should throw NotFoundException if the child does not exist', async () => {
+      mockChildRepository.findOne.mockResolvedValue(null);
+
+      await expect(service.deleteChild(1, 7)).rejects.toThrow(NotFoundException);
+    });
+
+    it('should throw UnauthorizedException if the child is not related to the user', async () => {
+      const existingChild = {
+        id: 5,
+        firstName: 'Елизавета',
+        lastName: 'Пупкина',
+        fatherName: 'Васильевна',
+        age: 3,
+        createdAt: new Date(Date.now() - 1000 * 60 * 60 * 24),
+        updatedAt: new Date(Date.now() - 1000 * 60 * 60 * 24),
+        parent: { id: 1 },
+      }; // Child belongs to another user
+
+      mockChildRepository.findOne.mockResolvedValue(existingChild);
+
+      await expect(service.deleteChild(2, 1)).rejects.toThrow(UnauthorizedException);
+    });
+  });
+
+
+  describe('getChildById', () => {
+    it('should return the child if it exists and is related to the user', async () => {
+      const parentUser = {
+        id: 1,
+        firstName: 'Вася',
+        lastName: 'Пупкин',
+        fatherName: 'Петрович',
+        age: 30,
+        children: new Array(2).fill({}),
+      };
+      const existingChild = {
+        id: 5,
+        firstName: 'Елизавета',
+        lastName: 'Пупкина',
+        fatherName: 'Васильевна',
+        age: 3,
+        createdAt: new Date(Date.now() - 1000 * 60 * 60 * 24),
+        updatedAt: new Date(Date.now() - 1000 * 60 * 60 * 24),
+        parent: parentUser,
+      };
+
+      mockChildRepository.findOne.mockResolvedValue(existingChild);
+
+      const result = await service.getChildById(parentUser.id, existingChild.id);
+      expect(result).toEqual(existingChild);
+      expect(mockChildRepository.findOne).toHaveBeenCalledWith({
+        where: { id: existingChild.id },
+        relations: ['parent']
+      });
+    });
+
+    it('should throw NotFoundException if the child does not exist', async () => {
+      mockChildRepository.findOne.mockResolvedValue(null);
+
+      await expect(service.getChildById(1, 7)).rejects.toThrow(NotFoundException);
+    });
+
+    it('should throw UnauthorizedException if the child is not related to the user', async () => {
+      const existingChild = {
+        id: 5,
+        firstName: 'Елизавета',
+        lastName: 'Пупкина',
+        fatherName: 'Васильевна',
+        age: 3,
+        createdAt: new Date(Date.now() - 1000 * 60 * 60 * 24),
+        updatedAt: new Date(Date.now() - 1000 * 60 * 60 * 24),
+        parent: { id: 1 },
+      };
+
+      mockChildRepository.findOne.mockResolvedValue(existingChild);
+
+      await expect(service.getChildById(2, 5)).rejects.toThrow(UnauthorizedException);
+    });
+  });
+
 });
--- src/family/services/user/user.service.spec.ts
+++ src/family/services/user/user.service.spec.ts
@@ -1,12 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from '../../entities';
+import { NotFoundException } from '@nestjs/common';
 
 describe('UserService', () => {
   let service: UserService;
+  let mockUserRepository: Partial<Record<keyof Repository<User>, jest.Mock>>;
 
   beforeEach(async () => {
+    // Implement mock repository methods
+    mockUserRepository = {
+      find: jest.fn(),
+      findOneBy: jest.fn(),
+      create: jest.fn(),
+      save: jest.fn(),
+      preload: jest.fn(),
+      delete: jest.fn(),
+    }
+
+    // Testing module with mock repository
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository,
+        }
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
@@ -15,4 +37,153 @@ describe('UserService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('getAllUsers', () => {
+    it('should return an array of users', async () => {
+      const testUsers = [
+        {
+          id: 1,
+          firstName: 'Вася',
+          lastName: 'Пупкин',
+          fatherName: 'Петрович',
+          age: 30
+        },
+        {
+          id: 2,
+          firstName: 'Марья',
+          lastName: 'Гавриловна',
+          fatherName: 'Харитонова',
+          age: 24
+        },
+        {
+          id: 3,
+          firstName: 'Василий',
+          lastName: 'Алибабаевич',
+          fatherName: 'Можно просто Вася',
+          age: 41
+        }
+      ];
+
+      // Emulate repostitory return
+      mockUserRepository.find.mockResolvedValue(testUsers);
+
+      // Method call result 
+      const result = await service.getAllUsers();
+
+      // Check that service returns what repository provides, and verify that it was called
+      expect(result).toEqual(testUsers);
+      expect(mockUserRepository.find).toHaveBeenCalled();
+    });
+  });
+
+  describe('getUserById', () => {
+    it('should retrieve and return a single user', async () => {
+      const testUser = {
+        id: 1,
+        firstName: 'Вася',
+        lastName: 'Пупкин',
+        fatherName: 'Петрович',
+        age: 30
+      };
+      // Emulate repostitory return
+      mockUserRepository.findOneBy.mockResolvedValue(testUser);
+
+      // Method call result
+      const result = await service.getUserById(1);
+
+      // Check that service returns data found, and verify that it was called correctly
+      expect(result).toEqual(testUser);
+      expect(mockUserRepository.findOneBy).toHaveBeenCalledWith({ id: 1 });
+    });
+
+    it('should throw NotFoundException if the user is not found', async () => {
+      // Emulate empty return from the DB
+      mockUserRepository.findOneBy.mockResolvedValue(null);
+
+      // Ensure it throws correctly
+      await expect(service.getUserById(1)).rejects.toThrow(NotFoundException);
+    });
+  });
+
+  describe('addUser', () => {
+    it('should create and save a new user', async () => {
+      // Mock dto and user entity
+      const createUserDto = { firstName: 'Иван', lastName: 'Иванов', fatherName: 'Иванович', age: 30 };
+      const newUser = { ...createUserDto, id: 1 };
+
+      // Emulate repository response
+      mockUserRepository.create.mockReturnValue(newUser);
+      mockUserRepository.save.mockReturnValue(newUser);
+
+      // Method call result
+      const result = await service.addUser(createUserDto);
+
+      // Check result and call arguments
+      expect(result).toEqual(newUser);
+      expect(mockUserRepository.create).toHaveBeenCalledWith(createUserDto);
+      expect(mockUserRepository.save).toHaveBeenCalledWith(newUser);
+    });
+  });
+
+  describe('updateUser', () => {
+    it('should update and return a user', async () => {
+      // Mock dto and entities
+      const updateUserDto = { firstName: 'Василий', lastName: 'Алибабаевич', age: 32 };
+      const currentUser = {
+        firstName: 'Иван',
+        lastName: 'Иванов',
+        fatherName: 'Иванович',
+        age: 30,
+        createdAt: new Date(Date.now() - 1000 * 60 * 60 * 24),
+        updatedAt: new Date(Date.now() - 1000 * 60 * 60 * 24),
+      };
+      const updatedUser = { ...currentUser, updateUserDto, updateAt: new Date() };
+
+      // Emulate repository calls
+      mockUserRepository.findOneBy.mockResolvedValue(currentUser);
+      mockUserRepository.preload.mockResolvedValue(updatedUser);
+      mockUserRepository.save.mockResolvedValue(updatedUser);
+
+      // Method call result
+      const result = await service.updateUser(1, updateUserDto);
+
+      // Check result and call arguments
+      expect(result).toEqual(updatedUser);
+      expect(mockUserRepository.findOneBy).toHaveBeenCalledWith({ id: 1 });
+      expect(mockUserRepository.preload).toHaveBeenCalledWith(
+        {
+          ...updateUserDto,
+          id: 1,
+          createdAt: currentUser.createdAt,
+          updatedAt: expect.any(Date),
+        }
+      );
+    });
+
+    it('should throw NotFoundException if user does not exist', async () => {
+      // Emulate empty return from the DB
+      mockUserRepository.findOneBy.mockResolvedValue(undefined);
+      // Check for the error
+      await expect(service.updateUser(1, { lastName: 'Пупкин' })).rejects.toThrow(NotFoundException);
+    });
+  });
+
+  describe('deleteUser', () => {
+    it('should delete a user', async () => {
+      // Emulate successfull deletion
+      mockUserRepository.delete.mockResolvedValue({ affected: 1 });
+
+      await service.deleteUser(1);
+
+      expect(mockUserRepository.delete).toHaveBeenCalledWith(1);
+    });
+
+    it('should throw NotFoundException if no user is found to delete', async () => {
+      // Emulate failed deletion
+      mockUserRepository.delete.mockResolvedValue({ affected: 0 });
+
+      await expect(service.deleteUser(1)).rejects.toThrow(NotFoundException);
+    });
+  });
+
 });
